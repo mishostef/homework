08566f625e1e1982c22befdfd206ab00998c4fed
mihail.stefanov <mihail.stefanov@amusnet.com>
2022-10-19 12:21:58 +0300

    extracted functions

--- js/summary.js
+++ js/summary.js
@@ -7,63 +7,46 @@ console.log('expenses', expensesArray);
 console.log('budget', budgetsArray);
 const summary = {};
 
-const budgetTransformed = {}
-budgetsArray.forEach(b => {
-    const [month, year] = b[0].split('.');
-    const income = Number(b[1]);
-    const budget = Number(b[2]);
-    console.log(`month=${month}   year=${year}`);
-    if (budgetTransformed.hasOwnProperty(year)) {
-        (budgetTransformed[year])[month] = [0, 0, 0, 0, 0, 0, budget, income];
-    } else {
-        budgetTransformed[year] = { [month]: [0, 0, 0, 0, 0, 0, budget, income] }
-    }
-});
+const budgetTransformed = transformBudget();
 console.log(JSON.stringify(budgetTransformed));
 
 
-expensesArray.forEach(exp => {
-    const [day, month, year] = exp[0].split('.');
-    const category = exp[2];
-    const categoryIndex = categories.indexOf(category);
-    const amount = Number(exp[3]);
-    console.log(budgetTransformed[year])
-    const monthSummaryArray = (budgetTransformed[year])[month];
-    if (monthSummaryArray) {//if there is budget for that month
-        monthSummaryArray[categoryIndex] += amount;
-        const totalSpentIndex = 5;
-        monthSummaryArray[totalSpentIndex] += amount;
-        const budgetOverrunsIndex = 6;
-        monthSummaryArray[budgetOverrunsIndex] = monthSummaryArray[budgetOverrunsIndex] - amount > 0 ? 0 :
-            amount - monthSummaryArray[budgetOverrunsIndex]
-
-    }
-});
-
+addExpensesToBudget();
 console.log(JSON.stringify(budgetTransformed));
 
-const year = 2018;
-const sortedKeys = Object.keys(budgetTransformed[year]).sort((a, b) => months.indexOf(a) - months.indexOf(b));
 
-console.log(sortedKeys);
-sortedKeys.forEach(key=>console.log)
+function addExpensesToBudget() {
+    expensesArray.forEach(exp => {
+        const [day, month, year] = exp[0].split('.');
+        const category = exp[2];
+        const categoryIndex = categories.indexOf(category);
+        const amount = Number(exp[3]);
+        console.log(budgetTransformed[year]);
+        const monthSummaryArray = (budgetTransformed[year])[month];
+        if (monthSummaryArray) { //if there is budget for that month
+            monthSummaryArray[categoryIndex] += amount;
+            const totalSpentIndex = 5;
+            monthSummaryArray[totalSpentIndex] += amount;
+            const budgetOverrunsIndex = 6;
+            monthSummaryArray[budgetOverrunsIndex] = monthSummaryArray[budgetOverrunsIndex] - amount > 0 ? 0 :
+                amount - monthSummaryArray[budgetOverrunsIndex];
 
+        }
+    });
+}
 
-// expenses.forEach(expense => {
-//     const month = expense[0].split('.')[1];
-//     const category = expense[2];
-//     const spent = Number(expense[3]);
-//     if (summary.hasOwnProperty(category)) {
-//         console.log('summary[category]= ',summary[category])
-//         if (summary[category].hasOwnProperty(month)) {
-//             (summary[category])[month] += spent;
-//         } else {
-//             (summary[category])[month] = spent;
-//         }
-//     } else {
-//         summary[category] = {};
-//         (summary[category])[month] = spent;
-//     }
-//     console.log(JSON.stringify(category));
-// });
-// console.log(JSON.stringify(summary));
+function transformBudget() {
+    const budgetTransformed = {};
+    budgetsArray.forEach(b => {
+        const [month, year] = b[0].split('.');
+        const income = Number(b[1]);
+        const budget = Number(b[2]);
+        console.log(`month=${month}   year=${year}`);
+        if (budgetTransformed.hasOwnProperty(year)) {
+            (budgetTransformed[year])[month] = [0, 0, 0, 0, 0, 0, budget, income];
+        } else {
+            budgetTransformed[year] = { [month]: [0, 0, 0, 0, 0, 0, budget, income] };
+        }
+    });
+    return budgetTransformed;
+}
