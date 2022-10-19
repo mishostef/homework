065b07897d1c3c8c4fcbbc076aef902fa0fdc5e3
mihail.stefanov <mihail.stefanov@amusnet.com>
2022-10-19 15:02:14 +0300

    added getTableData

--- js/summary.js
+++ js/summary.js
@@ -14,6 +14,33 @@ console.log(JSON.stringify(budgetTransformed));
 addExpensesToBudget();
 console.log(JSON.stringify(budgetTransformed));
 
+const table = getTableData(budgetTransformed);
+console.log('table=', JSON.stringify(table));
+
+function getTableData(budget, start = 0) {
+    const table = [[], [], [], [], [], []];
+    const sums = [0, 0, 0, 0, 0, 0];
+    const end = start + 3;
+    const allYearsBudgets = Object.values(budget);
+    console.log(allYearsBudgets);
+    for (let i = 0; i < allYearsBudgets.length; i++) {
+        const yearBudget = allYearsBudgets[i];
+        console.log(yearBudget);
+        const sortedMonths = Object.keys(yearBudget).sort((a, b) => months.indexOf(a) - months.indexOf(b));
+        console.log('sorted months=', sortedMonths);
+        for (let j = 0; j < sortedMonths.length; j++) {
+            if (start === end) return table;
+            const month = sortedMonths[i];
+            const monthBudget = yearBudget[month];
+            console.log('month = ', month, ' monthBudget = ', monthBudget);
+            for (let k = 0; k < 6; k++) {
+                table[k].push(monthBudget[k]);
+            }
+            start++;
+        }
+    }
+}
+
 
 function addExpensesToBudget() {
     expensesArray.forEach(exp => {
@@ -23,7 +50,7 @@ function addExpensesToBudget() {
         const amount = Number(exp[3]);
         console.log(budgetTransformed[year]);
         const monthSummaryArray = (budgetTransformed[year])[month];
-        if (monthSummaryArray) { //if there is budget for that month
+        if (monthSummaryArray) { //if there is a budget for that month
             monthSummaryArray[categoryIndex] += amount;
             const totalSpentIndex = 5;
             monthSummaryArray[totalSpentIndex] += amount;
@@ -34,6 +61,7 @@ function addExpensesToBudget() {
         }
     });
 }
+
 
 function transformBudget() {
     const budgetTransformed = {};
