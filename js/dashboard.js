c1305d623123005e3dfa5420ff82d4f26f938a6e
mihail.stefanov <mihail.stefanov@amusnet.com>
2022-10-20 09:49:26 +0300

    visualize dashboard results

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,4 @@
-import { setRecord, getRecord } from "./utils.js";
+import { getRecord } from "./utils.js";
 const budgetsArray = [...getRecord('budget').values()];
 const expensesArray = [...getRecord('records').values()];
 console.log(budgetsArray);
@@ -8,6 +8,12 @@ const budgetsums = getBugetSums(budgetsArray);
 console.log(JSON.stringify(budgetsums));
 const expenses = getBreakdown(expensesArray);
 console.log(JSON.stringify(expenses));
+const spent = expenses.Total;
+const remaining = Math.max(budgetsums.budget - spent, 0);
+const savings = Math.max(budgetsums.income - spent, 0);
+const arr = [spent, remaining, savings, ...Object.values(expenses)];
+console.log(arr);
+[...document.querySelectorAll('.cat-row span.row.value')].forEach((x, i) => x.textContent = arr[i]);
 
 function getBugetSums(budgetsArray) {
     let income = 0;
@@ -20,17 +26,17 @@ function getBugetSums(budgetsArray) {
 }
 
 function getBreakdown(expensesArray) {
-    const expenses = {};
+    const expenses = {
+        Utilities: 0, Groceries: 0,
+        Entertainment: 0, Transport: 0, Other: 0, Total: 0
+    };
     expensesArray.forEach(expense => {
         const category = expense[2];
         const spent = Number(expense[3]);
         console.log('category=', category);
         console.log('spent=', spent);
-        if (expenses.hasOwnProperty(category)) {
-            expenses[category] += spent;
-        } else {
-            expenses[category] = spent;
-        }
+        expenses[category] += spent;
+        expenses.Total += spent;
     })
     return expenses;
 }
